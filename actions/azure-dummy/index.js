d71dba94d5c71f83c725f265b30140d9100211ed
spaliwal <spaliwal@adobe.com>
2020-11-04 16:23:55 +0530

    Add dummy endpoint for GW deployment

---
+++ actions/azure-dummy/index.js
@@ -0,0 +1,24 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const { AzureDummyTvm } = require('../../lib/impl/AzureDummyTvm')
+const azureDummyTvm = new AzureDummyTvm()
+
+/**
+ * @param {object} params the input params
+ * @returns {Promise<object>} tvm response
+ */
+async function main (params) {
+  return azureDummyTvm.processRequest(params)
+}
+
+exports.main = main
---
+++ test/unit/actions/azure-dummy.test.js
@@ -0,0 +1,24 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+const azureDummyAction = require('../../../actions/azure-dummy')
+
+const { AzureDummyTvm } = require('../../../lib/impl/AzureDummyTvm')
+jest.mock('../../../lib/impl/AzureDummyTvm')
+
+beforeEach(() => {
+  AzureDummyTvm.prototype.processRequest.mockReset()
+})
+test('azure-dummy action has a main function and calls AzureDummyTvm.processRequest', async () => {
+  const fakeParams = { a: { nested: 'param' }, another: 'param' }
+  await azureDummyAction.main(fakeParams)
+  expect(AzureDummyTvm.prototype.processRequest).toHaveBeenCalledWith(fakeParams)
+})
