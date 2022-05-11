3de26d9b2615cf46a07274722a1f5c98cd10348f
Michael Goberling <mgoberling@Michaels-MacBook-Pro-3.local>
2022-05-11 12:15:24 -0400

    linter errors

--- actions/utils.js
+++ actions/utils.js
@@ -1,21 +1,22 @@
 /**
- * PromiseAll replacement for an object with key-values of promises. Returns promise 
- * data with the appropriate key name. 
- * @param {Object} obj 
- * @returns {Object}
+ * PromiseAll replacement for an object with key-values of promises. Returns promise
+ * data with the appropriate key name.
+ *
+ * @param {object} obj Key-value promises to resolve
+ * @returns {object} Resolved promises with values assigned to original promise keys
  */
-function objectPromise(obj) {
-    return Promise.all(
-        Object
-            .keys(obj)
-            .map(key => Promise.resolve(obj[key]).then(val => ({ key: key, val: val })))
-    ).then(items => {
-        let result = {};
-        items.forEach(item => result[item.key] = item.val);
-        return result;
-    });
+function objectPromise (obj) {
+  return Promise.all(
+    Object
+      .keys(obj)
+      .map(key => Promise.resolve(obj[key]).then(val => ({ key: key, val: val })))
+  ).then(items => {
+    const result = {}
+    items.forEach(item => { result[item.key] = item.val })
+    return result
+  })
 }
 
 module.exports = {
-    objectPromise
+  objectPromise
 }
